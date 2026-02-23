bb38c8dbdf54926ea1e7e21ac41ea48e0dde03ba
Sadmanuzzaman-dev <sadmanuzzaman.78@gmail.com>
2026-02-23 23:27:13 +0600

    "set up config"

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
 
 app = FastAPI(
     title= "Interactive Story Generator API",
     description= "API for creating dynamic, user-driven adventure stories",
     version="1.0.1",
     docs_url="/docs",
-    redoc_url="redoc",
+    redoc_url="/redoc",
 )   
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ["*"],
+    allow_origins= settings.ALLOW_ORIGINS,
     allow_credentials= True,
     allow_methods= ["*"],
-    allow_header= ["*"],
+    allow_headers= ["*"],
 )
 
 
@@ -22,7 +23,7 @@ if __name__ == "__main__":
     import uvicorn  
     uvicorn.run(
         "main:app",
-        host= "0.0.0.0",
+        host= "127.0.0.1",
         port= 5001,
         reload= True
 
