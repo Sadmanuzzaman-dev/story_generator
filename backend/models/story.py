4837e0175de932d0bac5690fd7639d95e9cbdd5a
Sadmanuzzaman-dev <sadmanuzzaman.78@gmail.com>
2026-02-24 23:30:56 +0600

    "database ready"

---
+++ backend/models/story.py
@@ -0,0 +1,6 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean, JSON
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from db.database import Base
+
